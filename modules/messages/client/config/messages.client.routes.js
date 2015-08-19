86c7eb7fe2d9cad3664b5281ce59e6767b0a4942
michaschwab <me@michaschwab.de>
2015-08-19 16:16:37 -0400

    got the messages dropdown menu to work, but the links arent working properly yet

--- modules/messages/client/config/messages.client.routes.js
+++ modules/messages/client/config/messages.client.routes.js
@@ -5,25 +5,26 @@ angular.module('messages').config(['$stateProvider',
 	function($stateProvider) {
 		// Messages state routing
 		$stateProvider.
-		state('listMessages', {
-			url: '/messages',
-			templateUrl: 'modules/messages/views/list-messages.client.view.html'
-		}).
-		state('createMessage', {
-			url: '/messages/to/:toUserId',
-			templateUrl: 'modules/messages/views/create-message.client.view.html'
-		}).
-		state('viewMessage', {
-			url: '/messages/:chatroomId',
-			templateUrl: 'modules/messages/views/view-message.client.view.html'
-		}).
-		state('viewChat', {
-			url: '/chats/:chatroomId',
-			templateUrl: 'modules/messages/views/chatroom.client.view.html'
-		}).
-		state('editMessage', {
-			url: '/messages/:messageId/edit',
-			templateUrl: 'modules/messages/views/edit-message.client.view.html'
-		});
+			state('messages.list', {
+				url: '/messages',
+				templateUrl: 'modules/messages/views/list-messages.client.view.html'
+			})
+			.state('messages.create', {
+				url: '/messages/create',
+				templateUrl: 'modules/messages/views/create-message.client.view.html'
+			})
+
+			.state('messages.createByUserId', {
+				url: '/messages/to/:toUserId',
+				templateUrl: 'modules/messages/views/create-message.client.view.html'
+			}).
+			state('messages.viewChatroom', {
+				url: '/chats/:chatroomId',
+				templateUrl: 'modules/messages/views/chatroom.client.view.html'
+			}).
+			state('messages.edit', {
+				url: '/messages/:messageId/edit',
+				templateUrl: 'modules/messages/views/edit-message.client.view.html'
+			});
 	}
 ]);
