6d01c6e7ebf11b89346dc2682c51b0ebe5db8e12
michaschwab <gjplickscoconutthunderstormb>
2015-09-05 17:07:36 -0400

    started a content list

---
+++ modules/contents/client/controllers/contents.client.controller.js
@@ -0,0 +1,100 @@
+'use strict';
+
+angular.module('contents').controller('ContentsController',
+    function($scope, $stateParams, Courses, Sourcetypes, Sources, Segments, $timeout, $location, Concepts)
+    {
+        $scope.courseId = $stateParams.courseId;
+        $scope.sourceTypeMap = {};
+        $scope.sourceTypes = [];
+        $scope.sourceSegments = {};
+        $scope.conceptMap = {};
+
+        Concepts.query(function(concepts)
+        {
+            $scope.concepts = concepts;
+
+            concepts.forEach(function(concept)
+            {
+                $scope.conceptMap[concept._id] = concept;
+            });
+        });
+
+        Sources.query(function(sources)
+        {
+            $scope.sources = sources;
+
+            Sourcetypes.query(function(sourceTypes)
+            {
+                $scope.sourceTypes = sourceTypes;
+
+                sourceTypes.forEach(function(sourceType)
+                {
+                    $scope.sourceTypeMap[sourceType._id] = sourceType;
+                });
+
+                Segments.query({courses:$stateParams.courseId}).$promise.then(function(segments)
+                {
+                    $scope.segments = segments;
+
+
+                    sources.forEach(function(source)
+                    {
+                        segments.filter(function(segment)
+                        {
+                            return segment.source === source._id;
+                        }).forEach(function(segment)
+                        {
+                            if($scope.sourceSegments[source._id] === undefined)
+                            {
+                                $scope.sourceSegments[source._id] = [];
+                            }
+                            $scope.sourceSegments[source._id].push(segment);
+                        });
+                    });
+
+                    /*sources.filter(function(source)
+                    {
+                        return source.type == $scope.sourceType._id;
+                    }).forEach(function(source)
+                    {
+                        $scope.sources.push(source);
+                    });*/
+                });
+
+                /*sources.forEach(function(source)
+                {
+                    if(!$scope.sourceTypeMap[source.type])
+                    {
+                        $scope.sourceTypeMap[source.type] = Sourcetypes.get
+                    }
+                });*/
+            });
+
+
+        });
+
+        $scope.getSegmentsNumberShort = function(num, source)
+        {
+            if(!$scope.sourceSegments[source._id] || num > $scope.sourceSegments[source._id].length)
+            {
+                //console.log($scope.sourceSegments[source._id].length);
+                num = !$scope.sourceSegments[source._id] ? 0 : $scope.sourceSegments[source._id].length;
+            }
+
+            return new Array(num);
+        };
+
+        $scope.getConceptsNumberShort = function(num, source)
+        {
+            if(!$scope.sourceSegments[source._id] || !$scope.sourceSegments[source._id].concepts)
+            {
+                num = 0;
+            }
+            else if(num > $scope.sourceSegments[source._id].concepts.length)
+            {
+                num = $scope.sourceSegments[source._id].concepts.length;
+            }
+
+            return new Array(num);
+        };
+    });
