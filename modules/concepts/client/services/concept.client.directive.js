af1e60bf5fd1b2e2e121d6708ca041433a681fa4
michaschwab <gjplickscoconutthunderstormb>
2015-09-21 14:38:52 -0400

    fixed some scope problems with showing the segments in the panel

--- modules/concepts/client/services/concept.client.directive.js
+++ modules/concepts/client/services/concept.client.directive.js
@@ -3,11 +3,14 @@
 angular.module('concepts').directive('concept', function(RecursionHelper) {
     return {
         restrict: "E",
-        scope: {concept: '='},
+        scope: {
+            concept: '='
+        },
         templateUrl: 'modules/concepts/views/panel/concept.client.view.html',
         replace: true,
         compile: function(element) {
             return RecursionHelper.compile(element, function(scope, iElement, iAttrs, controller, transcludeFn){
+                scope.courseScope = angular.element('.course-view').scope();
                 // Define your normal link function here.
                 // Alternative: instead of passing a function,
                 // you can also pass an object with
