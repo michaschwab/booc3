616e6d9b5726282fd9f7c0585951a122fbe46c15
michaschwab <gjplickscoconutthunderstormb>
2015-10-13 17:13:34 -0400

    built the client side controller for feedback sending

--- modules/core/client/controllers/feedbackModal.client.controller.js
+++ modules/core/client/controllers/feedbackModal.client.controller.js
@@ -1,13 +1,38 @@
 angular.module('conceptdependencies').controller('FeedbackModalController',
-    function ($scope, Courses, $modalInstance)
+    function ($scope, Courses, $modalInstance, $http)
     {
         //$scope.dependency = dependency;
         $scope.feedbackMode = 'course';
         $scope.technicalProblemOn = false;
         $scope.course = null;
         $scope.courses = Courses.query();
+        $scope.courseContent = '';
+        $scope.websiteContent = '';
 
-        $scope.ok = function () {
+        $scope.ok = function ()
+        {
+            var feedback = { type: $scope.feedbackMode };
+
+            if($scope.feedbackMode == 'course')
+            {
+                feedback.courseId = $scope.course._id;
+                feedback.content = $scope.courseContent;
+            }
+            else if($scope.feedbackMode == 'website')
+            {
+                feedback.technical = $scope.technicalProblemOn;
+                feedback.content = $scope.websiteContent;
+            }
+
+            $http.post('/api/feedback/send', feedback).success(function (response) {
+                /*$scope.credentials = null;
+                $scope.success = response.message;*/
+
+            }).error(function (response) {
+                /*$scope.credentials = null;
+                $scope.error = response.message;*/
+            });
+
             $modalInstance.close();
         };
 
