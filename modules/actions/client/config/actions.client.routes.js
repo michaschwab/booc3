7513b180c276166705c3fa555b3c78a64e82e1fb
michaschwab <gjplickscoconutthunderstormb>
2015-10-14 16:50:50 -0400

    built a frontend for the action list

---
+++ modules/actions/client/config/actions.client.routes.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// Setting up route
+angular.module('actions').config(['$stateProvider',
+  function ($stateProvider) {
+    // Articles state routing
+    $stateProvider
+      .state('actions', {
+        abstract: true,
+        url: '/actions',
+        template: '<ui-view/>',
+        data: {
+          roles: ['admin']
+        }
+      })
+      .state('actions.list', {
+        url: '',
+        templateUrl: 'modules/actions/views/list.client.view.html'
+      });
+  }
+]);
