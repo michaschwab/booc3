752128baa44d2330f27c065cac280c9289975f8d
michaschwab <gjplickscoconutthunderstormb>
2015-10-26 18:23:47 -0400

    course selection in backups now working

--- modules/backups/client/controller/backups.client.controller.js
+++ modules/backups/client/controller/backups.client.controller.js
@@ -4,4 +4,8 @@ angular.module('actions').controller('BackupsController',
     function($scope, $stateParams, $location, Authentication, Actions, $interval, Undo, Users, Courses)
     {
         $scope.courses = Courses.query();
+        $scope.courseSelect = function(course)
+        {
+            $scope.course = course;
+        };
     });
--- modules/courses/client/services/course-select.client.directive.js
+++ modules/courses/client/services/course-select.client.directive.js
@@ -1,7 +1,22 @@
 angular.module('courses')
     .directive('courseSelect', function() {
+
+        function link($scope, element, attrs)
+        {
+            //console.log(attrs, $scope);
+
+            $scope.onChange = function(course)
+            {
+                if(attrs.ngChange)
+                {
+                    $scope[attrs.ngChange](course);
+                }
+            }
+        }
+
         return {
             restrict: 'E',
-            templateUrl: 'modules/courses/views/course-select.client.view.html'
+            templateUrl: 'modules/courses/views/course-select.client.view.html',
+            link: link
         };
     });
