82d89724a4cc0c76693bc7122ff54dff7f8315c0
michaschwab <gjplickscoconutthunderstormb>
2016-02-22 17:24:10 -0500

    no longer using socket io for the logs so there is less overhead for logging things

--- modules/logs/client/services/logs.client.service.js
+++ modules/logs/client/services/logs.client.service.js
@@ -1,7 +1,7 @@
 'use strict';
 
 //Conceptdependencies service used to communicate Conceptdependencies REST endpoints
-angular.module('logs').factory('Log', ['$socketResource', '$http',
+angular.module('logs').factory('Log', ['$resource', '$http',
 	function($resource, $http) {
 		var Log = $resource('/api/logs/:logId', { logId: '@_id'
 		}, {
