a958ea244d38bf443d15b74790e52ad2e996f602
michaschwab <gjplickscoconutthunderstormb>
2016-03-02 15:24:02 -0500

    now the tag create & edit page embeds the list of emojis

--- modules/tags/client/controllers/tags.client.controller.js
+++ modules/tags/client/controllers/tags.client.controller.js
@@ -13,7 +13,6 @@ angular.module('tags').controller('TagsController',
         {
             if($stateParams.tagId)
             {
-                // todo: load from db and fill $scope.tag with it
                 $scope.tag = Tag.get({ tagId: $stateParams.tagId });
             }
             else
@@ -21,6 +20,25 @@ angular.module('tags').controller('TagsController',
                 $scope.tag = {};
             }
         };
+
+        $scope.$watch('tag.icon', function()
+        {
+            // This is to make sure pasting the <i class="em em-aquarius"></i> code from emoji-css works and is translated into just the css.
+            var pattern = /<i class="([a-z0-9-_ ]+)"><\/i>/i;
+
+            if($scope.tag && $scope.tag.icon)
+            {
+                var found = $scope.tag.icon.match(pattern);
+
+                if(found && found.length)
+                {
+                    if(found[1])
+                    {
+                        $scope.tag.icon = found[1];
+                    }
+                }
+            }
+        });
 
         $scope.create = function()
         {
