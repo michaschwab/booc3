4956a0be4e1e6147ed383c6cc4a9e2a4cbb1bfc6
michaschwab <gjplickscoconutthunderstormb>
2016-03-03 14:11:00 -0500

    added delete functionality to tags

--- modules/tags/client/services/tag.client.service.js
+++ modules/tags/client/services/tag.client.service.js
@@ -2,7 +2,7 @@
 'use strict';
 
 //Courses service used to communicate Tag REST endpoints
-angular.module('courses').factory('Tag', ['$resource',
+angular.module('courses').factory('Tag', ['$socketResource',
     function($resource) {
         return $resource('/api/tags/:tagId', { tagId: '@_id'
         }, {
