a419f40fd5f989d7139223e6c8b0ed4215c8517d
michaschwab <gjplickscoconutthunderstormb>
2016-06-16 13:05:19 -0400

    addition to last commit: allow cross origin

--- config/lib/express.js
+++ config/lib/express.js
@@ -96,10 +96,11 @@ module.exports.initMiddleware = function (app) {
     inMemory: true
   }));
 
-  /*app.all(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
-  });*/
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
   /*app.use(function(req, res, next) {
     var user = auth(req);
